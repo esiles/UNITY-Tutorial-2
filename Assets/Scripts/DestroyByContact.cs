2daf26a347abebe088c2f82b9fd77dbfac7f54ba
esiles <eriksiles@gmail.com>
2016-10-02 00:49:24 -0700

    Commit spaceblasters

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	private Transform t;
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		t = GetComponent<Transform> ();
+
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (gameController == null) 
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Instantiate(explosion, t.position, t.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+
+	}
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, ZMax;
+}
+
+public class MovementScript : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Boundary boundary;
+	public float tilt;
+	public float speed;
+	private AudioSource audio;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire;
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire)
+		{
+			audio = GetComponent<AudioSource> ();
+			audio.Play ();
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation); 
+
+		}
+	}
+
+
+	void Start () 
+	{
+
+		rb = GetComponent<Rigidbody>();
+	
+	}
+	   void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement*speed; 
+
+		rb.position = new Vector3 
+		(
+				Mathf.Clamp(rb.position.x,boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp(rb.position.z,boundary.zMin, boundary.ZMax)
+		);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
